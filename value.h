09a415a8afd2ea71418ce4455345d88e9e9d2c39
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-10-15 00:12:18 +0330

    Renamed, Refactored: added `DIY_` prefix to macros

--- value.h
+++ value.h
@@ -6,29 +6,29 @@
 #include <stdint.h>
 #include <string.h>
 
-#define QNAN ((uint64_t)0x7ffc000000000000)
-#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define DIY_QNAN ((uint64_t)0x7ffc000000000000)
+#define DIY_SIGN_BIT ((uint64_t)0x8000000000000000)
 
-#define TAG_NIL 1
-#define TAG_BOOLEAN 2
-#define TAG_OBJECT 3
+#define DIY_TAG_NIL 1
+#define DIY_TAG_BOOLEAN 2
+#define DIY_TAG_OBJECT 3
 
-#define NIL_VAL ((diy_value_t)(QNAN | TAG_NIL))
-#define TRUE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 1))
-#define FALSE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 0))
-#define OBJECT_VAL(obj_ptr)                                                   \
+#define DIY_NIL_VAL ((diy_value_t)(QNAN | TAG_NIL))
+#define DIY_TRUE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 1))
+#define DIY_FALSE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 0))
+#define DIY_OBJECT_VAL(obj_ptr)                                               \
   (diy_value_t) (SIGN_BIT | QNAN | TAG_OBJECT | (uint64_t)(uintptr_t)(obj_ptr))
 
-#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
-#define IS_NIL(value) ((value) == NIL_VAL)
-#define IS_BOOLEAN(value)                                                     \
+#define DIY_IS_NUMBER(value) (((value) & QNAN) != QNAN)
+#define DIY_IS_NIL(value) ((value) == NIL_VAL)
+#define DIY_IS_BOOLEAN(value)                                                 \
   (((value) & (QNAN | TAG_BOOLEAN)) == (QNAN | TAG_BOOLEAN))
-#define IS_OBJECT(value)                                                      \
+#define DIY_IS_OBJECT(value)                                                  \
   (((value) & (QNAN | TAG_OBJECT)) == (QNAN | TAG_OBJECT))
 
-#define AS_OBJECT(value) ((diy_obj_t *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
-#define AS_BOOLEAN(value) ((value) & 1)
-
+#define DIY_AS_OBJECT(value)                                                  \
+  ((diy_obj_t *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+#define DIY_AS_BOOLEAN(value) ((value) & 1)
 
 typedef uint64_t diy_value_t;
 
