8df8f710270b7c30ebf0783ec6094a59d1b36845
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-10-15 00:03:06 +0330

    Refactored: some small refactoring vis-a-vis order of macro definitions

--- value.h
+++ value.h
@@ -13,23 +13,22 @@
 #define TAG_BOOLEAN 2
 #define TAG_OBJECT 3
 
-#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+#define NIL_VAL ((diy_value_t)(QNAN | TAG_NIL))
+#define TRUE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 1))
+#define FALSE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 0))
+#define OBJECT_VAL(obj_ptr)                                                   \
+  (diy_value_t) (SIGN_BIT | QNAN | TAG_OBJECT | (uint64_t)(uintptr_t)(obj_ptr))
 
+#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
 #define IS_NIL(value) ((value) == NIL_VAL)
 #define IS_BOOLEAN(value)                                                     \
   (((value) & (QNAN | TAG_BOOLEAN)) == (QNAN | TAG_BOOLEAN))
 #define IS_OBJECT(value)                                                      \
   (((value) & (QNAN | TAG_OBJECT)) == (QNAN | TAG_OBJECT))
 
-#define AS_OBJECT(value) ((Obj *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+#define AS_OBJECT(value) ((diy_obj_t *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
 #define AS_BOOLEAN(value) ((value) & 1)
 
-#define NIL_VAL ((diy_value_t)(QNAN | TAG_NIL))
-#define TRUE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 1))
-#define FALSE_VAL ((diy_value_t)(QNAN | TAG_BOOLEAN | 0))
-
-#define OBJECT_VAL(obj_ptr)                                                   \
-  (diy_value_t) (SIGN_BIT | QNAN | TAG_OBJECT | (uint64_t)(uintptr_t)(obj_ptr))
 
 typedef uint64_t diy_value_t;
 
